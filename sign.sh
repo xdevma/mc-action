6241fdd78888c339ff1061c519d5062bf811a63a
Mohamed Ez-zarghili <8616968+ezzarghili@users.noreply.github.com>
2021-03-12 19:04:07 +0100

    test

--- sign.sh
+++ sign.sh
@@ -1,4 +1,4 @@
 echo "Signed artifacts"
 cd dist/$1
-cat ../artifacts.list | xargs -I {} bash -c "gpg --output {}.asc --sign {}"
+cat ./artifacts.list | xargs -I {} bash -c "gpg --output {}.asc --sign {}"
 find ./
