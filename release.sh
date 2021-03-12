f99963d16cf0e9fe33f6085cab3104698a871aeb
Mohamed Ez-zarghili <8616968+ezzarghili@users.noreply.github.com>
2021-03-12 22:55:28 +0100

    releaser

--- release.sh
+++ release.sh
@@ -1,1 +1,10 @@
-echo "release artifacts"
+echo "release artifacts $3"
+echo "<promoteRequest>" > promote.xml
+echo "<data>" >> promote.xml
+echo "<stagedRepositoryId>$3</stagedRepositoryId>" >> promote.xml
+echo "<description>Release</description>" >> promote.xml
+echo "<targetRepositoryId>maven2</targetRepositoryId>" >> promote.xml
+echo "</data>" >> promote.xml
+echo "</promoteRequest>" >> promote.xml
+
+curl --fail -X POST -d @promote.xml -H "Content-Type:application/xml" -H "Authorization:Basic $1" -H "Content-Type:application/xml" https://oss.sonatype.org/service/local/staging/profiles/$2/promote
