7b2875fcdd8f603cd88bf45a707accad02d912fa
Mohamed Ez-zarghili <8616968+ezzarghili@users.noreply.github.com>
2021-03-12 18:25:01 +0100

    test

--- pull.sh
+++ pull.sh
@@ -1,2 +1,5 @@
 echo "Pull artifacts for $1"
+cd dist/$1
 find . -type f | grep -v "md5\|sha1$" | sed -E s'@./@@' > ../artifacts.list
+cat ../artifacts.list | xargs -I {} bash -c "gpg --output {}.asc --sign {}"
+find ./
