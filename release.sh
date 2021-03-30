bff9a6c7720811cb2f31e5986581a7a8a045dfb8
Mohamed Ez-zarghili <8616968+ezzarghili@users.noreply.github.com>
2021-03-30 22:26:19 +0100

    test

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ echo "<promoteRequest>" > promote.xml
 echo "<data>" >> promote.xml
 echo "<stagedRepositoryId>$3</stagedRepositoryId>" >> promote.xml
 echo "<description>Release</description>" >> promote.xml
-echo "<targetRepositoryId>releases</targetRepositoryId>" >> promote.xml
+echo "<targetRepositoryId>release</targetRepositoryId>" >> promote.xml
 echo "</data>" >> promote.xml
 echo "</promoteRequest>" >> promote.xml
 cat promote.xml
